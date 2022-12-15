c15660e7ab2943fa32ecd75c355fd7d8e5fc9846
Tomás <114664692+moronosoutotomas@users.noreply.github.com>
2022-12-15 12:49:27 +0100

    Delete tarefa4 directory

--- tarefa4/escripito.js
+++
@@ -1,5 +0,0 @@
-ducias = prompt("Querido usuario, cantos ovos leva a túa tortilla?")
-parseInt(ducias);
-var numerito = parseInt(ducias);
-var ovos = parseInt(numerito / 12);
-alert("A túa tortilla leva " + ovos + " ducias de ovos, que pedazo tortilla!");
